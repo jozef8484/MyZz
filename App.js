f50c6c03449f8d9c3698d95d8be9ed6d787617e5
Jozef Janikovic <jozef.janikovic@qpaynet.com>
2016-03-21 15:28:25 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items:{ html: 'my first Zz version 2!'},
+    launch: function() {
+        //Write app code here
+console.log('First');
+        //API Docs: https://help.rallydev.com/apps/2.0/doc/
+    }
+});
