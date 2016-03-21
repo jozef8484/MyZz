c343f735c051538e25386c08d5c88c7712a1e000
Jozef Janikovic <jozef.janikovic@qpaynet.com>
2016-03-21 15:56:23 +0000

    add iterations

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html: 'my first Zz version 2!'},
+    items:[{ html: 'my first Zz version 2!'}, {xtype: 'rallyiterationcombobox'},{xtype: 'rallyreleasecombobox'}],
+    layout: 'hbox',
     launch: function() {
         //Write app code here
 console.log('First');
